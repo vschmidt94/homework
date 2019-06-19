390efd720498ea5a0fc803406f3f87762a608ff8
Vaughan Schmidt <vaughan.schmidt@gmail.com>
2019-06-19 16:40:33 -0700

    Adds manage.py module for flask

--- backend/app/main/config.py
+++ backend/app/main/config.py
@@ -2,14 +2,17 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'the_very_secret_key')
     DEBUG = False
 
-class DevelopementConfig(Config):
+
+class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_api_main.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class TestingConfig(Config):
     DEBUG = True
@@ -18,15 +21,16 @@ class TestingConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PRESERVE_CONTEXT_ON_EXCEPTION = False
 
+
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_api_test.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PRESERVE_CONTEXT_ON_EXCEPTION = False
 
-config_by_name = dict(
-    dev = DevelopmentConfig,
-    test = TestingConfig,
-    prod = ProductionConfig )
+
+config_by_name = {'dev': DevelopmentConfig,
+                  'test': TestingConfig,
+                  'prod': ProductionConfig}
 
 key = Config.SECRET_KEY
