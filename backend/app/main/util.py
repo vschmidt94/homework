8872a8a58383026bf7e67ca4f43e5350f751286d
Vaughan Schmidt <vaughan.schmidt@gmail.com>
2019-06-19 17:16:51 -0700

    Adds user service and DTO utility class for user data

---
+++ backend/app/main/util.py
@@ -0,0 +1,16 @@
+from flask_restplus import Namespace, fields
+
+
+class UserDto:
+    """
+    User Data Transfer Object
+
+    Responsible for transfering user data between objects.
+    """
+    api = Namespace('user', description='user related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='user email address'),
+        'username': fields.String(required=True, description='user username'),
+        'password': fields.String(required=True, description='user password'),
+        'public_id': fields.String(description='user Identifier')
+    })
